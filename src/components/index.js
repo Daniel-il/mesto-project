07f726d9b5d85bba723a91f9cc648dea39b75fab
Aleksandra Klochkova <SandraSurgut@yandex.ru>
2022-03-26 17:53:12 +0500

    подготовлен класс FormValidator

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,8 @@ import {
   changeButton,
   popupTypeAvatar,
   profileImage,
-  apiConfig
+  apiConfig,
+  validationSettings,
 } from "./constants.js";
 import { submitFormAddCard, cardsList, addCard } from "./cards";
 import {
@@ -22,8 +23,8 @@ import {
   profileName,
   cardForm,
 } from "./modals";
-import { enableValidation, validationSettings } from "./validate";
 import Api from "./api.js";
+import FormValidator from "./validate";
 
 const api = new Api(apiConfig);
 let userId;
@@ -47,11 +48,15 @@ popups.forEach((popup) => {
     }
   });
 });
-enableValidation(validationSettings);
+const formList = Array.from(document.querySelectorAll(".form"));
+formList.forEach((formElement) => {
+  const formValidator = new FormValidator(validationSettings, formElement);
+  formValidator.enableValidation();
+});
+
 Promise.all([api.getUserData(), api.getCards()])
   .then(([userData, cards]) => {
-    (profileName.textContent = userData.name),
-      (profileDescription.textContent = userData.about);
+    (profileName.textContent = userData.name), (profileDescription.textContent = userData.about);
     profileImage.src = userData.avatar;
     userId = userData._id;
     cards.forEach((card) => {
