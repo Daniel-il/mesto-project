3ea4b6852538a87a2d4483e154f16069e4098c00
Aleksandra Klochkova <SandraSurgut@yandex.ru>
2022-04-03 20:38:52 +0500

    реализация класса PopupWithForm

--- src/components/index.js
+++ src/components/index.js
@@ -11,41 +11,76 @@ import {
   cardsListSelector,
   cardSelector,
   validationSettings,
-  popupTypeImageSelector
+  popupTypeImageSelector,
 } from "./constants";
-import { submitFormAddCard } from "./card";
-import {
-  profileForm,
-  submitFormProfile,
-  setProfileValues,
-  submitAvatarLink,
-  avatarForm,
-  profileDescription,
-  profileName,
-  cardForm,
-} from "./modals";
+import { profileDescription, profileName } from "./modals";
 import Api from "./api";
 import Card from "./card";
 import Section from "./section";
 import FormValidator from "./validate";
 import Popup from "./Popup";
+import PopupWithForm from "./PopupWithForm";
 
 const api = new Api(apiConfig);
-const popupTypeAddCard = new Popup(popupTypeAddCardSelector);
-const popupTypeProfile = new Popup(popupTypeProfileSelector);
-const popupTypeAvatar = new Popup(popupTypeAvatarSelector);
-const popupTypeImage= new Popup(popupTypeImageSelector);
+
+const popupTypeProfile = new PopupWithForm(
+  popupTypeProfileSelector,
+  {
+    isLoadingText: "Сохранeние...",
+    isLoadedText: "Сохранить",
+  },
+  (inputValues) => {
+    popupTypeProfile.renderLoading(true);
+    api
+      .changeUserData({
+        name: inputValues["form-name"],
+        about: inputValues["form-description"],
+      })
+      .then(() => {
+        profileName.textContent = inputValues["form-name"];
+        profileDescription.textContent = inputValues["form-description"];
+        popupTypeProfile.close();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        popupTypeProfile.renderLoading(false);
+      });
+  }
+);
+popupTypeProfile.setEventListener();
+
+const popupTypeAvatar = new PopupWithForm(
+  popupTypeAvatarSelector,
+  {
+    isLoadingText: "Обновление...",
+    isLoadedText: "Обновить",
+  },
+  (inputValues) => {
+    popupTypeAvatar.renderLoading(true);
+    api
+      .changeAvatar({
+        link: inputValues["form-avatar"],
+      })
+      .then(() => {
+        profileImage.src = inputValues["form-avatar"];
+        popupTypeAvatar.close();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        popupTypeAvatar.renderLoading(false);
+      });
+  }
+);
+popupTypeAvatar.setEventListener();
+const popupTypeImage = new Popup(popupTypeImageSelector);
 
 let userId;
-avatarForm.addEventListener("submit", submitAvatarLink);
-profileForm.addEventListener("submit", submitFormProfile);
-addButton.addEventListener("click", () => popupTypeAddCard.open());
 editButton.addEventListener("click", () => {
   popupTypeProfile.open();
-  setProfileValues();
+  popupTypeProfile.setInputValue({ name: "form-name", value: profileName.textContent });
+  popupTypeProfile.setInputValue({ name: "form-description", value: profileDescription.textContent });
 });
 changeButton.addEventListener("click", () => popupTypeAvatar.open());
-cardForm.addEventListener("submit", submitFormAddCard);
 
 const formList = Array.from(document.querySelectorAll(".form"));
 formList.forEach((formElement) => {
@@ -58,13 +93,44 @@ Promise.all([api.getUserData(), api.getCards()])
     (profileName.textContent = userData.name), (profileDescription.textContent = userData.about);
     profileImage.src = userData.avatar;
     userId = userData._id;
-    const cardsList = new Section({items: api.getCards(), renderer: (item) => {
-      const card = new Card({name: item.name, link: item.link, _id : item._id, likes: item.likes, owner: item.owner._id}, userId, cardSelector, () => {
-        popupTypeImage.open();
-      });
-      const cardElement = card.generate();
-      cardsList.addItem(cardElement)
-    }}, cardsListSelector)
-    cardsList.renderItems(cards)
+    const cardsList = new Section(
+      {
+        items: cards,
+        renderer: (item) => {
+          const card = new Card(
+            { name: item.name, link: item.link, _id: item._id, likes: item.likes, owner: item.owner._id },
+            userId,
+            cardSelector,
+            () => {
+              popupTypeImage.open();
+            }
+          );
+          const cardElement = card.generate();
+          return cardElement;
+        },
+      },
+      cardsListSelector
+    );
+    cardsList.renderItems(cards);
+    const popupTypeAddCard = new PopupWithForm(
+      popupTypeAddCardSelector,
+      {
+        isLoadingText: "Создание...",
+        isLoadedText: "Создать",
+      },
+      (inputValues) => {
+        popupTypeAddCard.renderLoading(true);
+        api
+          .sendCard(inputValues["form-name"], inputValues["form-description"])
+          .then((res) => {
+            cardsList.addItem(res);
+            popupTypeAddCard.close();
+          })
+          .catch((err) => console.log(err))
+          .finally(() => popupTypeAddCard.renderLoading(false));
+      }
+    );
+    popupTypeAddCard.setEventListener();
+    addButton.addEventListener("click", () => popupTypeAddCard.open());
   })
   .catch((err) => console.log(err));
--- src/components/modals.js
+++ src/components/modals.js
@@ -21,51 +21,9 @@ export const jobInput = inputContainer.querySelector(
 const cardForm = document.querySelector(".form_type_add-card");
 const profileForm = document.querySelector(".form_type_profile");
 const avatarForm = document.querySelector(".form_type_avatar");
-const avatarSubmitButton = document.querySelector(".form__submit-button_type_avatar")
-function setProfileValues() {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileDescription.textContent;
-}
-function submitAvatarLink(evt) {
-  renderLoading(true, avatarForm, "Обновление...");
-  evt.preventDefault();
-  api.changeAvatar({
-    link: avatarInput.value,
-  })
-    .then(() => {
-      profileImage.src = avatarInput.value;
-      evt.target.reset();
-      closePopup(popupTypeAvatar);
-      avatarSubmitButton.disabled = true
-      avatarSubmitButton.classList.add('form__submit-button_inactive')
-    })
-    .catch((err) => console.log(err))
-    .finally(() => {
-      renderLoading(false, avatarForm, "Обновить");
-    });
-}
-function submitFormProfile(evt) {
-  renderLoading(true, profileForm, "Сохранeние...");
-  evt.preventDefault();
-  api.changeUserData({
-    name: profileName.textContent,
-    about: profileDescription.textContent,
-  })
-  .then(() => {
-    profileName.textContent = nameInput.value;
-    profileDescription.textContent = jobInput.value;
-    closePopup(popupTypeProfile);
-  })
-    .catch((err) => console.log(err))
-    .finally(() => {
-      renderLoading(false, profileForm, "Сохранить");
-    });
-}
+
 export {
   profileForm,
-  submitFormProfile,
-  setProfileValues,
-  submitAvatarLink,
   avatarForm,
   cardForm,
 };
